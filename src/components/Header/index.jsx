dcbe3737a7b51b48d7012420a3d837047457b5e7
IgorPierre <igor.pierre45@gmail.com>
2023-09-08 11:33:54 -0300

    feat: ✨ create the header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import { HeaderArea } from "./style"
+
+function Header() {
+    return(
+        <HeaderArea>
+            <span>To Do!</span>
+        </HeaderArea>
+    )
+}
+
+export default Header
---
+++ src/components/Header/style.js
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const HeaderArea = styled.header`
+    background-color: var(--royal-blue);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 1.5rem 0;
+
+    span {
+        font-weight: 900;
+        font-size: 2rem;
+        color: var(--white);
+    }
+`
