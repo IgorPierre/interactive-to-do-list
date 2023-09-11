8bcc6f48b979275ea5cbe8ffd70d6d3d47694132
IgorPierre <igor.pierre45@gmail.com>
2023-09-11 13:17:50 -0300

    feat: ✨cheate the FinishButton component

---
+++ src/components/Buttons/FinishButton.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FinishButton = styled.button`
+    border: 1px solid var(--dark-blue);
+    border-radius: .2rem;
+    width: 20px;
+    height: 20px;
+    margin-right: .5rem;
+`
