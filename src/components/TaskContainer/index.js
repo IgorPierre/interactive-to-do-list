2e7f46aacaeebcf11fe5fef33093a9961ca22a3b
IgorPierre <igor.pierre45@gmail.com>
2023-09-11 09:30:21 -0300

    feat: ✨ create the TaskContainer component

---
+++ src/components/TaskContainer/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const TaskContainer = styled.div`
+    position: absolute;
+    top: 25%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 50%;
+`
