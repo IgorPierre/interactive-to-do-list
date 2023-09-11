0fe93c8eb1799e7b7c3427ce53f86a38c84d590c
IgorPierre <igor.pierre45@gmail.com>
2023-09-11 09:29:48 -0300

    feat: ✨ create the TaskCard component

---
+++ src/components/TaskCard/index.js
@@ -0,0 +1,24 @@
+import { styled } from "styled-components";
+
+export const TaskCard = styled.div`
+    background: rgba( 255, 255, 255, 0.35 );
+    box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+    backdrop-filter: blur( 13.5px );
+    -webkit-backdrop-filter: blur( 13.5px );
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 10px;
+    border: 1px solid rgba( 255, 255, 255, 0.18 );
+    width: 100%;
+    margin: .8rem;
+    padding: .5rem 1rem;
+
+    input[type="checkbox"] {
+        margin-right: .5rem;
+    }
+
+    .is-finished {
+        text-decoration: line-through;
+    }
+`
