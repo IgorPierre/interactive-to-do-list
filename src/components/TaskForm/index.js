8af0d42ae07c9994c4e49017d08acf8a93fb9ed7
IgorPierre <igor.pierre45@gmail.com>
2023-09-11 13:19:49 -0300

    feat: ✨ create the TaskForm component

---
+++ src/components/TaskForm/index.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const TaskForm = styled.form`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin: .8rem;
+    margin-bottom: 2rem;
+
+    & input {
+        color: var(--white);
+        font-size: 1rem;
+        width: 80%;
+        background: none;
+        border: none;
+        border-bottom: 2px solid var(--dark-blue);
+        padding: .3rem .5rem;
+    }
+    & input::placeholder {
+        color: var(--white);
+    }
+`
