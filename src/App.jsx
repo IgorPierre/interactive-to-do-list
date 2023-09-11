52e4724c0bd0b4178154cc026033db43819c69bd
IgorPierre <igor.pierre45@gmail.com>
2023-09-11 09:27:35 -0300

    feat: 💫 add the trash button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
 import './App.css'
 
+import { LiaTrashAlt } from 'react-icons/lia'
+
+import { TaskContainer } from './components/TaskContainer'
+import { useState } from 'react'
+import { TaskCard } from './components/TaskCard'
+import { TrashButton } from './components/Buttons/TrashButton'
+
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Tirar o lixo",
+      isFinished: false,
+    },
+    {
+      id: 2,
+      text: "Comprar pão",
+      isFinished: true,
+    },
+  ])
+
+  const [checked, setChecked] = useState("checked");
+
+  const handleCheckboxChange = (value) => {
+    return (!value);
+  };
+
   return (
     <>
-      <h1>Olá, mundo!</h1>
+      <TaskContainer>
+        {tasks.map((task) => (
+          <TaskCard>
+            <label htmlFor="">
+              <input 
+                type="checkbox"
+                checked={checked}
+              />
+              <span className={task.isFinished === true ? "is-finished" : ""} >{task.text}</span>
+            </label>
+            <TrashButton>
+              <LiaTrashAlt />
+            </TrashButton>
+          </TaskCard>
+        ))}
+      </TaskContainer>
     </>
   )
 }
---
+++ src/components/Buttons/TrashButton.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const TrashButton = styled.a`
+    text-decoration: none;
+    font-size: 1.4rem;
+    color: var(--white);
+
+    &:hover {
+        color: var(--red);
+    }
+`
