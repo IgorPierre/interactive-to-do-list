25e8d45c2c5e86877e4e3fe87284f5887c3e1249
IgorPierre <igor.pierre45@gmail.com>
2023-09-15 21:49:48 -0300

    feat: ✨ add tasks data

---
+++ Data/TasksData.js
@@ -0,0 +1,19 @@
+const TasksData = [
+    {
+        id: 1,
+        text: "Tirar o lixo",
+        isFinished: false,
+      },
+      {
+        id: 2,
+        text: "Comprar pão",
+        isFinished: true,
+      },
+      {
+          id: 3,
+          text: "Finalizar a Sprint EV-4",
+          isFinished: false,
+      },
+]
+
+export default TasksData
