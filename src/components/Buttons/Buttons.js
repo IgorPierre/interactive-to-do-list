40a56831675a0156cf061e37407b03426252327f
IgorPierre <igor.pierre45@gmail.com>
2023-09-15 21:48:33 -0300

    refactor: ♻️ groups all buttons in one file

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,14 +1,15 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const DefaultButton = styled.button`
     background-color: ${props => props.primary ? "var(--green)" : "var(--gray)"};
     color: var(--white);
     font-size: 1rem;
     padding: .3rem .5rem;
+    margin-left: 1rem;
     border: 1px solid var(--green);
     border-radius: .3rem;
     cursor: pointer;
-    transition: .8s;
+    transition: .4s;
 
     &::before {
         content: "${props => props.customContent || ''}";
@@ -19,5 +20,26 @@ export const DefaultButton = styled.button`
     &:hover {
         box-shadow: inset 150px 0 var(--white);
         color: var(--green);
+    }
+`
+
+export const FinishButton = styled.button`
+    border: 1px solid var(--dark-blue);
+    border-radius: .2rem;
+    width: 20px;
+    height: 20px;
+    margin-right: .5rem;
+`
+
+export const TrashButton = styled.a`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    font-size: 1.4rem;
+    color: var(--white);
+    
+    &:hover {
+        color: var(--red);
     }
 `
